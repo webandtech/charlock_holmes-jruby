823d64b06d1e0a59ba31d77de32217d599652edb
Aaron Severs <asevers@gmail.com>
2014-02-07 22:31:12 -0800

    update icu4j version

--- lib/charlock_holmes/version.rb
+++ lib/charlock_holmes/version.rb
@@ -1,5 +1,5 @@
 module CharlockHolmes
-  VERSION           = "0.1.2"
-  ICU_VERSION       = "51.1"
-  ICU_JAR_FILENAME  = "icu4j-51_1.jar"
+  VERSION           = "0.1.3"
+  ICU_VERSION       = "52.1"
+  ICU_JAR_FILENAME  = "icu4j-52_1.jar"
 end
